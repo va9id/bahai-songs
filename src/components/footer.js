04b203f4d79bbd1111eab15b5ea7746e869ce14e
vahido9 <vayeeded@gmail.com>
2025-02-06 20:08:12 -0500

    inject footer + header

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const footerHTML = `
+      <footer class="bg-body-tertiary text-center text-start mt-auto">
+        <div id="footer" class="text-center p-3">
+          © 2025 Copyright -
+          <a href="#">bahaisongs.ca</a> -
+          <a class="mr-1" href="https://github.com/va9id/bahai-songs"><i class="bi bi-github"></i></a>
+        </div>
+      </footer>
+    `;
+    document.body.insertAdjacentHTML("beforeend", footerHTML);
+});
+
---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname;
+    let basePath = '';
+    if (currentPath.includes('/src/pages/')) {
+        basePath = '../../';  // If in /src/pages/, go up 2 levels to root
+    }
+
+    const currentPage = currentPath.split('/').pop();
+
+    const headerHTML = `
+        <nav class="navbar navbar-expand bg-body-tertiary sticky-top">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link ${currentPage === 'index.html' ? 'active' : ''}" href="${basePath}index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ${currentPage === 'songs.html' ? 'active' : ''}" href="${basePath}src/pages/songs.html">Songs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ${currentPage === 'contact.html' ? 'active' : ''}" href="${basePath}src/pages/contact.html">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `;
+
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});

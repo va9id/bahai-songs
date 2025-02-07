451a879f341850ec240b4e5a2cad5379a679b9ec
vahido9 <vayeeded@gmail.com>
2025-02-07 10:53:58 -0500

    legal

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const footerHTML = `
       <footer class="bg-body-tertiary text-center text-start mt-auto">
         <div id="footer" class="text-center p-3">
+          <a id="about" href="/src/pages/about.html">About</a> -  
           <a id="legal" href="/src/pages/legal.html">Legal</a> -  
           © 2025 Copyright
           <a href="#">bahaisongs.ca</a> -

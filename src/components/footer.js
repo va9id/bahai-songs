07a50d08b5d8e603d8821e9b31986511176cadbe
vahido9 <vayeeded@gmail.com>
2025-02-07 11:18:32 -0500

    add contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const footerHTML = `
       <footer class="bg-body-tertiary text-center text-start mt-auto">
-        <div id="footer" class="text-center p-3">
-          <a id="about" href="/src/pages/about.html">About</a> -  
-          <a id="legal" href="/src/pages/legal.html">Legal</a> -  
-          © 2025 Copyright
-          <a href="#">bahaisongs.ca</a> -
+        <div id="footer" class="text-center p-2">
+          <a id="about" href="/src/pages/about.html">About</a> - 
+          <a id="legal" href="/src/pages/legal.html">Legal</a> - 
+          <a id="about" href="/src/pages/contact.html">Contact</a> - 
           <a class="mx-1" href="https://github.com/va9id/bahai-songs"><i class="bi bi-github"></i></a>
+          <div>© 2025 Copyright
+            <a href="#">bahaisongs.ca</a>
+          </div>
         </div>
       </footer>
     `;
